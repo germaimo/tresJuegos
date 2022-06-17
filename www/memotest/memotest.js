baabd8be284342b11eda5503e6c82a873b04272e
batimovil <german.figueroa@ort.edu.ar>
2022-06-17 04:46:43 -0300

    comienzo de cargado en html, primero comence con una tabla, pero no me parecio que valga la pena esa sintaxis, asi que fui por tags p, si bien es mas sencillo, el problema reside en que siempre presento a los pares juntos, y tengo que ordernarlos entre si, no se si armar un array antes con los valores duplicados, luego mezclarlos y finalmente mapear eso con los tags p, esa seria una alternativa, pero no me convence

--- www/memotest/memotest.js
+++ www/memotest/memotest.js
@@ -5,11 +5,24 @@ let game = {
     time: 0,
     difficulty: 1, // puede cambiar desde que comienza
     tablero: [
-        [0, 0, 0],
-        [0, 0, 0],
-        [0, 0, 0]
+        { id: 0, text: 'A' },
+        { id: 1, text: 'B' },
+        { id: 2, text: 'C' },
+        { id: 3, text: 'D' },
     ],
 };
+
+const shuffleArray = (array) => {
+    let counter = array.length;
+    while (counter > 0) {
+        let rnd = Math.floor(Math.random() * counter);
+        counter--;
+        let tmp = array[counter];
+        array[counter] = array[rnd];
+        array[rnd] = tmp;
+    }
+    return array;
+}
 
 let itemAcomparar = false; // lo hago separado por que no quiero acceder al objeto game constantemente
 
@@ -18,6 +31,15 @@ let itemAcomparar = false; // lo hago separado por que no quiero acceder al obje
 
 //se muestra html con cartas mezcladas
 
+const tablero = document.getElementById('tablero');
+
+const mezcla = shuffleArray(game.tablero);
+
+const cards = [...mezcla].map(({ id, text }) => (`<p> <span id=${id}>${text}</span> <span id=${id}>${text}</span> </p>`)).join('');
+
+tablero.innerHTML = cards;
+
+
 //se muestra boton comenzar
     //inicia timer
 
@@ -25,6 +47,6 @@ let itemAcomparar = false; // lo hago separado por que no quiero acceder al obje
 
 //al clickear sobre una imagen se chequea si hay algun valor seteado en itemAComparar guarda el valor para comparar 
 // si hay valor seteado se compara el itemAComparar con el segudo clickeado
-    // si esta comparacion es verdadera moves++ 
+    // si esta comparacion es verdadera moves++
     // se remueven estos items clickeados 
-     
+
